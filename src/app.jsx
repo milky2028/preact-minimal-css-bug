b59fe9c684c4848940863d5f4678bc7fa0d95b0f
Tyler <tylergross28@gmail.com>
2020-11-27 17:26:50 -0600

    detect react and preact

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import * as react from 'react';
 import { render } from 'react-dom';
 import styled from 'styled-components';
 
@@ -15,12 +15,16 @@ const Input = styled.input`
 `;
 
 function App() {
-  const [inputVal, setInputVal] = useState('');
-
-  useEffect(() => console.log(inputVal));
+  const [inputVal, setInputVal] = react.useState('');
 
   return (
     <div>
+      <h1>
+        {react.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED
+          .ReactCurrentOwner === undefined
+          ? 'Preact'
+          : 'React'}
+      </h1>
       <Input value={inputVal} onChange={(e) => setInputVal(e.target.value)} />
       <Label>
         Background should only be red when focused or when value is not an empty
